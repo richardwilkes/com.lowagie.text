70f6e3383f2c66493d916f24361342dc87110724
Richard Wilkes <wilkes@me.com>
2018-06-10 16:03:54 -0700

    Use bin instead of build

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ mkdir $BUILD_DIR
 
 javac -d $BUILD_DIR $(find src -name '*.java')
 
-jar --create --file=$MODULES_DIR/com.lowagie.text@${VERSION}.jar --module-version=${VERSION} -C $BUILD_DIR . -C resources .
+jar --create --file $MODULES_DIR/com.lowagie.text@${VERSION}.jar --module-version ${VERSION} -C $BUILD_DIR . -C resources .
 
 zip -r -9 -q $MODULES_DIR/com.lowagie.text@${VERSION}-src.zip src resources
 
